6d3b01e4d1279096125cebea45de3e13d2d76146
CdrThresh <taunwe79@gmail.com>
2023-04-05 14:50:53 -0500

    day 1, worked on printing simple statements, strings, operator types, variable assignment, and user input

---
+++ printing.py
@@ -0,0 +1,20 @@
+# Practice of printing text
+
+print("hello world");
+print( "you there" )
+
+#Strings/Variable assignment
+
+rock = "Malphite";
+print(rock);
+water = "aqua";
+print(water);
+paper = "Cardboard";
+del paper;
+paper = 6;
+print(type(paper));
+
+#Input 
+
+cup = input("What do you want to put in the cup? ");
+print(cup);
